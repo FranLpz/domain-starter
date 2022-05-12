d9585906fd5defcabb3aea8574f514fc5e960588
FranLpz <franciscoalopezd@gmail.com>
2022-05-12 00:35:38 -0500

    Render minted domains

--- src/App.js
+++ src/App.js
@@ -312,7 +312,44 @@ const App = () => {
 			</div>
 		)
 	}
-		return (
+
+	const renderMints = () => {
+		if (currentAccount && mints.length > 0) {
+			return (
+				<div className='mint-container'>
+					<p className='subtitle'>Recently minted domains!</p>
+					<div className='mint-list'>
+						{ mints.map((mint, index) => {
+							return (
+								<div className='mint-item' key={index}>
+									<div className='mint-row'>
+										<a className="link" href={`https://testnets.opensea.io/assets/mumbai/${CONTRACT_ADDRESS}/${mint.id}`} target="_blank" rel="noopener noreferrer">
+											<p className="underlined">{' '}{mint.name}{tld}{' '}</p>
+										</a>
+										{/* If mint.owner is currentAccount, add an "edit" button*/}
+										{ mint.owner.toLowerCase() === currentAccount.toLowerCase() ?
+											<button className="edit-button" onClick={() => editRecord(mint.name)}>
+												<img className="edit-icon" src="https://img.icons8.com/metro/26/000000/pencil.png" alt="Edit button" />
+											</button>
+										: null }
+									</div>
+									<p> {mint.record} </p>
+								</div>
+							)
+						})}
+					</div>
+				</div>
+			)
+		}
+	}
+
+	// This will take us into edit mode and show us the edit buttons!
+	const editRecord = (name) => {
+		console.log("Editing record for", name);
+		setEditing(true);
+		setDomain(name);
+	}
+
 	// This runs our function when the page loads.
 	useEffect(() => {
 		checkIfWalletIsConnected();
@@ -339,6 +376,9 @@ const App = () => {
 				{!currentAccount && renderNotConnectedContainer()}
 				{/* Render the input form if an account is connected */}
 				{currentAccount && renderInputForm()}
+				{/* Show mints */}
+				{mints && renderMints()}
+
 
         		<div className="footer-container">
 					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
