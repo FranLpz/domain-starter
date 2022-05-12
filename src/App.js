d3e9ed9c9a002421723825cd58e71c80e00c2b7d
FranLpz <franciscoalopezd@gmail.com>
2022-05-12 00:18:16 -0500

    🦊 Add and switch networks in MetaMask

--- src/App.js
+++ src/App.js
@@ -43,7 +43,49 @@ const App = () => {
 		} catch (error) {
 		  console.log(error)
 		}
-	  }
+	}
+
+	// Switch network funtion
+	const switchNetwork = async () => {
+		if(window.ethereum){
+			try {
+				// Try to switch to the Mumbai testnet
+				await window.ethereum.request({
+					method: 'wallet_switchEthereumChain',
+					params: [{ chainId: '0x13881' }], // Check networks.js for hexadecimal network ids
+				});
+			} catch (error) {
+				// This error code means that the chain we want has not been added to MetaMask
+				// In this case we ask the user to add it to their MetaMask
+				if (error.code === 4902) {
+					try {
+						await window.ethereum.request({
+							method: 'wallet_addEthereumChain',
+							params: [
+							{	
+								chainId: '0x13881',
+								chainName: 'Polygon Mumbai Testnet',
+								rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
+								nativeCurrency: {
+									name: "Mumbai Matic",
+									symbol: "MATIC",
+									decimals: 18
+								},
+								blockExplorerUrls: ["https://mumbai.polygonscan.com/"]
+							},
+							],
+						});
+					} catch (error) {
+						console.log(error);
+					}
+				}
+				console.log(error);
+			}
+		} else {
+			// If window.ethereum is not found then MetaMask is not installed
+			alert('MetaMask is not installed. Please install it to use this app: https://metamask.io/download.html');
+		}
+	}
 
 	// Gotta make sure this is async.
 	const checkIfWalletIsConnected = async () => {
@@ -146,6 +188,8 @@ const App = () => {
 			return (
 				<div className='connect-wallet-container'>
 					<h2>Please switch to Polygon Mumbai Testnet</h2>
+					{/* This button will call our switch network function */}
+					<button className='cta-button connect-wallet-button' onClick={switchNetwork}>Click here to switch</button>
 				</div>
 			)
 		}
